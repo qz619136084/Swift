b97dadc9365d69fea4a9c51e7b12b7fd4d543657
Rebirth <qz619136084@163.com>
2022-07-13 16:59:58 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by RebirthQ on 2022/7/13.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
